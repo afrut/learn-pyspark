500ddfbdcc2ee11e24589e025962ce50600fb287
eric373 <25942990+eric373@users.noreply.github.com>
2022-03-19 11:25:01 -0400

    Parametrize spark-submit script

--- scripts/ss.ps1
+++ scripts/ss.ps1
@@ -1,5 +1,8 @@
 Clear-Host
-spark-submit --master local `
-    --jars "D:\Spark\sqljdbc_10.2\enu\mssql-jdbc-10.2.0.jre8.jar" `
-    DataFrameDemo.py `
-    $Env:SPARK_HOME+"\README.md"
+if($args.Length -gt 0)
+{
+    spark-submit --master local `
+        --jars "D:\Spark\sqljdbc_10.2\enu\mssql-jdbc-10.2.0.jre8.jar" `
+        $args[0] `
+        $($Env:SPARK_HOME + "\README.md")
+}
